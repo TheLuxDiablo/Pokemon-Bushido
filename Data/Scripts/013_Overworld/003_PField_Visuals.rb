2533e7e51437f18d5120a1fa8da3ec4944494b34
CamThundaga <72353508+CamThundaga@users.noreply.github.com>
2021-02-28 00:17:32 -0800

    Hot Tub Jump + LOTS OF FIXES AND TWEAKS

--- Data/Scripts/013_Overworld/003_PField_Visuals.rb
+++ Data/Scripts/013_Overworld/003_PField_Visuals.rb
@@ -708,9 +708,9 @@ def pbStartOver(gameover=false)
   pbHealAll
   if $PokemonGlobal.pokecenterMapId && $PokemonGlobal.pokecenterMapId>=0
     if gameover
-      pbMessage(_INTL("\\w[]\\wm\\l[3]After the unfortunate defeat, you scurry back to a Pokémon Center."))
+      pbMessage(_INTL("\\w[]\\wm\\l[3]After the unfortunate defeat, you scurry back to the Hot Springs."))
     else
-      pbMessage(_INTL("\\w[]\\wm\\l[3]You scurry back to a Pokémon Center, protecting your exhausted Pokémon from any further harm..."))
+      pbMessage(_INTL("\\w[]\\wm\\l[3]You scurry back to the Hot Springs, protecting your exhausted Pokémon from any further harm..."))
     end
     pbCancelVehicles
     pbRemoveDependencies
