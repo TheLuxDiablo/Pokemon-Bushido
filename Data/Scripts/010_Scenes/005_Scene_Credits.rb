49f4309f11f7435106f6617999b0469b067fb247
CamThundaga <72353508+CamThundaga@users.noreply.github.com>
2021-03-13 21:39:26 -0800

    Credits setup

--- Data/Scripts/010_Scenes/005_Scene_Credits.rb
+++ Data/Scripts/010_Scenes/005_Scene_Credits.rb
@@ -1,15 +1,11 @@
 # Backgrounds to show in credits. Found in Graphics/Titles/ folder
-CreditsBackgroundList = ["credits12","credits6","credits9",
-  "credits8","credits26","credits14","credits27","credits23",
-  "credits28","credits25","credits20","credits29","credits13",
-  "credits7","credits15","credits17","credits18","credits16",
-  "credits19","credits10","credits21","credits11","credits22","credits24"]
-CreditsMusic          = "Credits"
+CreditsBackgroundList = ["backgroundbushido"]
+CreditsMusic          = "PMD-DontEverForget"
 CreditsScrollSpeed    = 2
-CreditsFrequency      = 3   # Number of seconds per credits slide
-CREDITS_OUTLINE       = Color.new(0,0,128, 255)
-CREDITS_SHADOW        = Color.new(0,0,0, 100)
-CREDITS_FILL          = Color.new(255,255,255, 255)
+CreditsFrequency      = 10   # Number of seconds per credits slide
+CREDITS_OUTLINE       = Color.new(121,39,39, 255)
+CREDITS_SHADOW        = Color.new(0,0,0, 120)
+CREDITS_FILL          = Color.new(244,205,205, 255)
 
 #==============================================================================
 # * Scene_Credits
@@ -52,36 +48,16 @@ class Scene_Credits
 # This next piece of code is the credits.
 #Start Editing
 CREDIT=<<_END_
-A game by Thundaga (Cameron R)
-
-Artists
-Voltseon
-Tristantine The Great
-Kristiano100
-TabletPillowLamp
-P-Plum
-
-Programmers
-GolisopodUser
-Seyuna
-Voltseon
-
-Shoutouts to the Thundaga Twitch Chat
-Aka the Thundagang
-Aka the Camfam
-Aka the Bolt Cult
+Pokémon Bushido
 
 
-Additional Amazing Art
-P-Plum
-ShadowPhil
-Design3D
-ShashuGreninja
-Dedbed
-Carlin
 
-Pokemon Splice Wiki
-ENLS
+A game by
+LuxDiablo<s>Thundaga
+Tristantine The Great<s>HauntedArtStudio
+
+
+
 
 A HUGE thank you to the resource creators
 of the Pokemon fangame community
@@ -122,147 +98,141 @@ This is a non-profit fan-made game.
 No copyright infringements intended.
 Please support the official games!
 
+Shoutouts to the Thundaga Twitch Chat
+Aka the Thundagang
+
 _END_
 #Stop Editing
 
-  def main
+def main
 #-------------------------------
 # Animated Background Setup
 #-------------------------------
-    @sprite = IconSprite.new(0,0)
-    @backgroundList = CreditsBackgroundList
-    @frameCounter = 0
-    # Number of game frames per background frame
-    @framesPerBackground = CreditsFrequency * Graphics.frame_rate
-    @sprite.setBitmap("Graphics/Titles/"+@backgroundList[0])
+  @sprite = IconSprite.new(0,0)
+  @backgroundList = CreditsBackgroundList
+  @frameCounter = 0
+  # Number of game frames per background frame
+  @framesPerBackground = CreditsFrequency * Graphics.frame_rate
+  @sprite.setBitmap("Graphics/Titles/"+@backgroundList[0])
 #------------------
 # Credits text Setup
 #------------------
-    plugin_credits = ""
-    PluginManager.plugins.each do |plugin|
-      pcred = PluginManager.credits(plugin)
-      plugin_credits << "\"#{plugin}\"\n"
-      if pcred.size >= 5
-        plugin_credits << pcred[0] + "\n"
-        i = 1
-        until i >= pcred.size
-          plugin_credits << pcred[i] + "<s>" + (pcred[i + 1] || "") + "\n"
-          i += 2
-        end
-      else
-        pcred.each do |name|
-          plugin_credits << name + "\n"
-        end
+  plugin_credits = ""
+  PluginManager.plugins.each do |plugin|
+    pcred = PluginManager.credits(plugin)
+    plugin_credits << "\"#{plugin}\" version #{PluginManager.version(plugin)}\n"
+    if pcred.size >= 5
+      plugin_credits << pcred[0] + "\n"
+      i = 1
+      until i >= pcred.size
+        plugin_credits << pcred[i] + "<s>" + (pcred[i + 1] || "") + "\n"
+        i += 2
       end
-      plugin_credits << "\n"
-    end
-    CREDIT.gsub!(/\{INSERTS_PLUGIN_CREDITS_DO_NOT_REMOVE\}/, plugin_credits)
-    credit_lines = CREDIT.split(/\n/)
-    credit_bitmap = Bitmap.new(Graphics.width,32 * credit_lines.size)
-    credit_lines.each_index do |i|
-      line = credit_lines[i]
-      line = line.split("<s>")
-      # LINE ADDED: If you use in your own game, you should remove this line
-      pbSetSystemFont(credit_bitmap) # <--- This line was added
-      xpos = -30
-      align = 2 # Centre align
-      linewidth = Graphics.width
-      for j in 0...line.length
-        if line.length>1
-          xpos = (j==0) ? -30 : -30 + Graphics.width/2
-          align = (j==0) ? 2 : 2 # Right align : left align
-          linewidth = Graphics.width/2 - 10
-        end
-        credit_bitmap.font.color = CREDITS_SHADOW
-        credit_bitmap.draw_text(xpos,i * 32 + 8,linewidth,32,line[j],align)
-        credit_bitmap.font.color = CREDITS_OUTLINE
-        credit_bitmap.draw_text(xpos + 2,i * 32 - 2,linewidth,32,line[j],align)
-        credit_bitmap.draw_text(xpos,i * 32 - 2,linewidth,32,line[j],align)
-        credit_bitmap.draw_text(xpos - 2,i * 32 - 2,linewidth,32,line[j],align)
-        credit_bitmap.draw_text(xpos + 2,i * 32,linewidth,32,line[j],align)
-        credit_bitmap.draw_text(xpos - 2,i * 32,linewidth,32,line[j],align)
-        credit_bitmap.draw_text(xpos + 2,i * 32 + 2,linewidth,32,line[j],align)
-        credit_bitmap.draw_text(xpos,i * 32 + 2,linewidth,32,line[j],align)
-        credit_bitmap.draw_text(xpos - 2,i * 32 + 2,linewidth,32,line[j],align)
-        credit_bitmap.font.color = CREDITS_FILL
-        credit_bitmap.draw_text(xpos,i * 32,linewidth,32,line[j],align)
+    else
+      pcred.each do |name|
+        plugin_credits << name + "\n"
       end
     end
-    @trim = Graphics.height/10
-    @realOY = -(Graphics.height-@trim)   # -430
-    @oyChangePerFrame = CreditsScrollSpeed*20.0/Graphics.frame_rate
-    @credit_sprite = Sprite.new(Viewport.new(0,@trim,Graphics.width,Graphics.height-(@trim*2)))
-    @credit_sprite.bitmap = credit_bitmap
-    @credit_sprite.z      = 9998
-    @credit_sprite.oy     = @realOY
-    @bg_index = 0
-    @zoom_adjustment = 1.0/$ResizeFactor
-    @last_flag = false
+    plugin_credits << "\n"
+  end
+  CREDIT.gsub!(/\{INSERTS_PLUGIN_CREDITS_DO_NOT_REMOVE\}/, plugin_credits)
+  credit_lines = CREDIT.split(/\n/)
+  credit_bitmap = Bitmap.new(Graphics.width,32 * credit_lines.size)
+  credit_lines.each_index do |i|
+    line = credit_lines[i]
+    line = line.split("<s>")
+    # LINE ADDED: If you use in your own game, you should remove this line
+    pbSetSystemFont(credit_bitmap) # <--- This line was added
+    xpos = 0
+    align = 1 # Centre align
+    linewidth = Graphics.width
+    for j in 0...line.length
+      if line.length>1
+        xpos = (j==0) ? 0 : 20 + Graphics.width/2
+        align = (j==0) ? 2 : 0 # Right align : left align
+        linewidth = Graphics.width/2 - 20
+      end
+      credit_bitmap.font.color = CREDITS_SHADOW
+      credit_bitmap.draw_text(xpos,i * 32 + 8,linewidth,32,line[j],align)
+      credit_bitmap.font.color = CREDITS_OUTLINE
+      credit_bitmap.draw_text(xpos + 2,i * 32 - 2,linewidth,32,line[j],align)
+      credit_bitmap.draw_text(xpos,i * 32 - 2,linewidth,32,line[j],align)
+      credit_bitmap.draw_text(xpos - 2,i * 32 - 2,linewidth,32,line[j],align)
+      credit_bitmap.draw_text(xpos + 2,i * 32,linewidth,32,line[j],align)
+      credit_bitmap.draw_text(xpos - 2,i * 32,linewidth,32,line[j],align)
+      credit_bitmap.draw_text(xpos + 2,i * 32 + 2,linewidth,32,line[j],align)
+      credit_bitmap.draw_text(xpos,i * 32 + 2,linewidth,32,line[j],align)
+      credit_bitmap.draw_text(xpos - 2,i * 32 + 2,linewidth,32,line[j],align)
+      credit_bitmap.font.color = CREDITS_FILL
+      credit_bitmap.draw_text(xpos,i * 32,linewidth,32,line[j],align)
+    end
+  end
+  @trim = Graphics.height/10
+  @realOY = -(Graphics.height-@trim)   # -430
+  @oyChangePerFrame = CreditsScrollSpeed*20.0/Graphics.frame_rate
+  @credit_sprite = Sprite.new(Viewport.new(0,@trim,Graphics.width,Graphics.height-(@trim*2)))
+  @credit_sprite.bitmap = credit_bitmap
+  @credit_sprite.z      = 9998
+  @credit_sprite.oy     = @realOY
+  @bg_index = 0
+  @zoom_adjustment = 1.0/$ResizeFactor
+  @last_flag = false
 #--------
 # Setup
 #--------
-    # Stops all audio but background music
-    previousBGM = $game_system.getPlayingBGM
-    pbMEStop
-    pbBGSStop
-    pbSEStop
+  # Stops all audio but background music
+  previousBGM = $game_system.getPlayingBGM
+  pbMEStop
+  pbBGSStop
+  pbSEStop
+  pbBGMFade(2.0)
+  pbBGMPlay(CreditsMusic)
+  Graphics.transition(20)
+  loop do
+    Graphics.update
+    Input.update
+    update
+    break if $scene != self
+  end
+  Graphics.freeze
+  @sprite.dispose
+  @credit_sprite.dispose
+  $PokemonGlobal.creditsPlayed = true
+  pbBGMPlay(previousBGM)
+end
+
+# Check if the credits should be cancelled
+def cancel?
+  if Input.trigger?(Input::C) && $PokemonGlobal.creditsPlayed
+    $scene = Scene_Map.new
+    pbBGMFade(1.0)
+    return true
+  end
+  return false
+end
+
+# Checks if credits bitmap has reached its ending point
+def last?
+  if @realOY > @credit_sprite.bitmap.height + @trim
+    $scene = ($game_map) ? Scene_Map.new : nil
     pbBGMFade(2.0)
-    pbBGMPlay(CreditsMusic)
-    Graphics.transition(20)
-  #  @sprite.opacity = 0
-  #  @credit_sprite.opacity = 0
-    loop do
-      Graphics.update
-      Input.update
-      break if update
-    #  break if $scene != self
-    end
-#    Graphics.freeze
-    (Graphics.frame_rate/2).times do
-      @sprite.opacity -= 255/(Graphics.frame_rate/2)
-      @credit_sprite.opacity -= 255/(Graphics.frame_rate/2)
-      Graphics.update
-    end
-    @sprite.dispose
-    @credit_sprite.dispose
-    $PokemonGlobal.creditsPlayed = true
-  #  pbBGMPlay(previousBGM)
-    return
+    return true
   end
+  return false
+end
 
-  # Check if the credits should be cancelled
-  def cancel?
-    if Input.trigger?(Input::C) && ($PokemonGlobal.creditsPlayed || $DEBUG)
-    #  $scene = Scene_Map.new
-      pbBGMFade(1.0)
-      return true
-    end
-    return false
+def update
+  @frameCounter += 1
+  # Go to next slide
+  if @frameCounter >= @framesPerBackground
+    @frameCounter -= @framesPerBackground
+    @bg_index += 1
+    @bg_index = 0 if @bg_index >= @backgroundList.length
+    @sprite.setBitmap("Graphics/Titles/"+@backgroundList[@bg_index])
   end
-
-  # Checks if credits bitmap has reached its ending point
-  def last?
-    if @realOY > @credit_sprite.bitmap.height + @trim
-    #  $scene = ($game_map) ? Scene_Map.new : nil
-      pbBGMFade(2.0)
-      return true
-    end
-    return false
-  end
-
-  def update
-    @frameCounter += 1
-    # Go to next slide
-    if @frameCounter >= @framesPerBackground
-      @frameCounter -= @framesPerBackground
-      @bg_index += 1
-      @bg_index = 0 if @bg_index >= @backgroundList.length
-      @sprite.setBitmap("Graphics/Titles/"+@backgroundList[@bg_index])
-    end
-    return true if cancel?
-    return true if last?
-    @realOY += @oyChangePerFrame
-    @credit_sprite.oy = @realOY
-    return false
-  end
+  return if cancel?
+  return if last?
+  @realOY += @oyChangePerFrame
+  @credit_sprite.oy = @realOY
+end
 end
