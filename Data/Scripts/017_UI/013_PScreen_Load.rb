7b00d1cc509da835cea830bdf8125cfc5165fd1b
CamThundaga <72353508+CamThundaga@users.noreply.github.com>
2022-03-16 21:31:18 -0700

    Tsuchi Xatu teleport and Journal fix in load menu (Seen -> Owned)

--- Data/Scripts/017_UI/013_PScreen_Load.rb
+++ Data/Scripts/017_UI/013_PScreen_Load.rb
@@ -87,7 +87,7 @@ class PokemonLoadPanel < SpriteWrapper
         textpos.push([_INTL("Chapter:"),16*2,56*2,0,TEXTCOLOR,TEXTSHADOWCOLOR])
         textpos.push([@trainer.chapter.to_s,103*2,56*2,1,TEXTCOLOR,TEXTSHADOWCOLOR])
         textpos.push([_INTL("Journal:"),16*2,72*2,0,TEXTCOLOR,TEXTSHADOWCOLOR])
-        textpos.push([@trainer.pokedexSeen.to_s,103*2,72*2,1,TEXTCOLOR,TEXTSHADOWCOLOR])
+        textpos.push([@trainer.pokedexOwned(2).to_s,103*2,72*2,1,TEXTCOLOR,TEXTSHADOWCOLOR])
         textpos.push([_INTL("Time:"),16*2,88*2,0,TEXTCOLOR,TEXTSHADOWCOLOR])
         hour = @totalsec / 60 / 60
         min  = @totalsec / 60 % 60
