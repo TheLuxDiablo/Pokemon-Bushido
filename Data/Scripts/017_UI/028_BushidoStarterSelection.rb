941affabb76d46256c6b5dbec4dff04d92595770
theluxdiablo <74836863+TheLuxDiablo@users.noreply.github.com>
2026-08-06 02:08:56 -0400

    turn off debug starters

--- Data/Scripts/017_UI/028_BushidoStarterSelection.rb
+++ Data/Scripts/017_UI/028_BushidoStarterSelection.rb
@@ -33,7 +33,7 @@ module BushidoStarterSelection
   # :BOTH   - Force random species and shininess
   #
   # Put this back to :OFF before release.
-  DEBUG_MODE = :BOTH
+  DEBUG_MODE = :OFF
 
   #=============================================================================
   # Regional forms
