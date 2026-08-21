2868ac486d885409bed2c9ed637eb48c55620f5e
Cameron R <72353508+CamThundaga@users.noreply.github.com>
2026-08-21 14:49:00 -0500

    Gen9 Hisui Balls Sprites (Poke/Great/Ultra)

--- Data/Scripts/023_PluginScripts/015_ItemFind.rb
+++ Data/Scripts/023_PluginScripts/015_ItemFind.rb
@@ -186,7 +186,7 @@ def pbReceiveItem(item, quantity = 1)
   return false if !item || item<=0 || quantity<1
   itemname = (quantity>1) ? PBItems.getNamePlural(item) : PBItems.getName(item)
   pocket = pbGetPocket(item)
-  meName = $PokemonBag.pbCanStore?(item,quantity) ? ((pbIsKeyItem?(item)) ? "HGSSGetKeyItem" : "HGSSGetItem") : ""
+  meName = $PokemonBag.pbCanStore?(item,quantity) ? ((pbIsKeyItem?(item)) ? "PLA 014 Caught a Pokemon!" : "PLA 036 Obtained a Key Item!") : ""
   if isConst?(item,PBItems,:KATANABASIC)
     meName = "BOTW-GetFanfare"
   elsif isConst?(item,PBItems,:KATANALIGHT)
