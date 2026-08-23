a2da46838289ad2a76a41d8fbeef8c864842bddc
theluxdiablo <74836863+TheLuxDiablo@users.noreply.github.com>
2026-08-23 16:46:51 -0400

    adding trainer indicators (without bushido effects)

--- Data/Scripts/001_Settings.rb
+++ Data/Scripts/001_Settings.rb
@@ -371,3 +371,9 @@ INITIAL_ENERGY = 3
 
 SUKIRO_SPAR_QUIZ_QUESTIONS = 25
 SUKIRO_REMATCH_CHAPTERBASED = true
+
+#===============================================================================
+# Trainer Sensor
+#===============================================================================
+TRAINER_SENSOR_ENABLED = true
+TRAINER_SENSOR_BUSHIDO_STYLE = true
